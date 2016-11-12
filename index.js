d9e32362dfc602aacc79441ff99c5e5643281532
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2016-11-12 14:55:47 -0200

    Add functions on server

--- index.js
+++ index.js
@@ -10,10 +10,47 @@ var startServer = function (done) {
     domain: 'localhost'
   })
 
-  // On connection event. When a client connects.
+// On connection event. When a client connects.
   server.on('connection', function (client) {
-   console.log("Cliente " + client.fid + " conectado !"); 
-  }) 
+    // That's the way you add mods to a given server.
+
+    // Allows the developer to register the jid against anything they want
+    client.on('register', function (opts, cb) {
+      console.log('REGISTER')
+      cb(true)
+    })
+
+    // Allows the developer to authenticate users against anything they want.
+    client.on('authenticate', function (opts, cb) {
+      console.log('server:', opts.username, opts.password, 'AUTHENTICATING')
+      cb(null, opts)
+      /*if (opts.password === 'secret') {
+        console.log('server:', opts.username, 'AUTH OK')
+        cb(null, opts)
+      } else {
+        console.log('server:', opts.username, 'AUTH FAIL')
+        cb(false)
+      } */
+    })
+
+    client.on('online', function () {
+      console.log('server:', client.jid.local, 'ONLINE')
+    }) 
+
+    // Stanza handling
+    client.on('stanza', function (stanza) {
+      console.log('server:', client.jid.local, 'stanza', stanza.toString())
+      var from = stanza.attrs.from
+      stanza.attrs.from = stanza.attrs.to
+      stanza.attrs.to = from
+      client.send(stanza)
+    })
+
+    // On Disconnect event. When a client disconnects
+    client.on('disconnect', function () {
+      console.log('server:', client.jid.local, 'DISCONNECT')
+    })  
+  })
 
   server.on('listening', done)
 }
