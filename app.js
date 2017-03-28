ead23e19e3330da9147121825dcf8652f4313670
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2017-03-27 21:12:27 -0300

    Add servidor multipart

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict'
+
+
+const Hapi = require('hapi');
+var fs     = require('fs');
+const server = new Hapi.Server();
+
+server.connection({ 
+  port: 8080,
+  host: 'localhost' 
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('mHelp API');
+    }
+});
+
+server.route({
+    method: 'POST',  
+    path: '/upload',  
+    config: {  
+    payload: {
+        maxBytes: 209715200,
+        output: 'stream',
+        parse: false
+    }
+    },
+      handler: function(request, reply) {
+          var multiparty = require('multiparty');
+          var form = new multiparty.Form();
+          form.parse(request.payload, function(err, fields, files) {
+              fs.readFile(files.photo.path, function (err, data){
+              var newPath = "uploads/" + files.photo.originalFilename;
+              
+              fs.writeFile(newPath, data, function (err) {
+        
+                if(err)
+                  return reply({'response':"Error"});
+                });         
+              });  
+              return reply({'response':"Saved"});
+          });
+      }
+    
+});
+
