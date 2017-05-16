cb652a7b31efb60282c33ef733457aa437f30885
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2017-05-16 19:55:29 -0300

    Alteracoes gerais

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const Hapi = require('hapi');
-var fs     = require('fs');
+const Hapi   = require('hapi');
 const server = new Hapi.Server();
-const Inert = require('inert');
+var fs       = require('fs');
+const Inert  = require('inert');
 
 server.connection({ 
   port: 8000 
@@ -13,9 +13,10 @@ server.start((err) => {
   if (err) {
       throw err;
   }
-  console.log(`Server running at: ${server.info.uri}`);
+  console.log(`Servidor rodando em: ${server.info.uri}`);
 });
 
+//Habilita o fornecimento de pastas e diretórios estáticos
 server.register(Inert, () => {});
 
 server.route({
@@ -29,26 +30,28 @@ server.route({
 server.route({
   method: 'POST',  
   path: '/upload',  
-  config: {  
+  config: {
+  //Configurações adicionais da carga útil que será recebida  
   payload: {
-      maxBytes: 209715200,
-      output: 'stream',
+      maxBytes: 209715200, //Tamanho máximo do arquivo
+      output: 'stream', 
       parse: false
   }
   },
     handler: function(request, reply) {
       var multiparty = require('multiparty');
       var form = new multiparty.Form();
-      form.parse(request.payload, function(err, fields, files) {
-        fs.readFile(files.file.path, function (err, data){
-            var newPath = "uploads/" + files.file.originalFilename;
-            
-            fs.writeFile(newPath, data, function (err) {
-              if(err)
-                return reply({'response':"Error"});
+      //Extrai o arquivo da requisição 
+      form.parse(request.payload, function(error, fields, files) {
+        fs.readFile(files.file.path, function (error, data){
+            var endereco = "uploads/" + files.file.originalFilename;
+            //Salva o arquivo no endereço especificado
+            fs.writeFile(endereco, data, function (error) {
+              if(error)
+                return reply({'response':"Erro"});
             });         
         });  
-        return reply({'response':"Saved"});
+        return reply({'response':"Salvo"});
       });
     }
     
@@ -56,14 +59,15 @@ server.route({
 
 server.route({  
   method: 'GET',
-  path: '/delete/{filename?}',
+  path: '/delete/{filename*}',
   handler: function(request, reply) {
     var path = "uploads/" + request.params.filename;
+    //Deleta o arquivo
     fs.unlink(path, function(err,data){
         if (err)
-          return reply({'response':"Error"});
-        
-        return reply({'response':"Deleted"});
+          return reply({'response':"Erro"});
+
+        return reply({'response':"Deletado"});
     });  
   }
   
@@ -71,7 +75,7 @@ server.route({
 
 server.route({  
   method: 'GET',
-  path: '/download/{param*}',
+  path: '/download/{filename*}',
   handler:{
     directory:{
       path: 'uploads'
